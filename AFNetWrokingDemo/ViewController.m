ddbc21a506d34f758923ad5e2761a091a3349bf2
MrZhangy <18600218483@163.com>
2015-06-01 23:49:11 +0800

    downloadFile

--- AFNetWrokingDemo/ViewController.m
+++ AFNetWrokingDemo/ViewController.m
@@ -27,26 +27,63 @@
 
 -(void)requestData
 {
-    
-//    NSDictionary*parameters=@{@"type":@"7",@"page":@"1",@"token":@"0c0e9a6e25d947fcaf7488d45b1ffea5-14274390",@"count":@"20",@"last":@"589328",@"mtype":@"2",@"vtype":@"1",@"pcursor":@"",@"os":@"android",@"client_key":@"6bcc9821",@"network":@"WIFI",@"sig":@"3e20376180f73316e0e6e4acdfd6c85d"};
-
-    
-    
     NSDictionary *parameters = @{@"type":@"7",@"page":@"1",@"token":@"0c0e9a6e25d947fcaf7488d45b1ffea5-14274390",@"count":@"20",@"last":@"589328",@"mtype":@"2",@"vtype":@"1",@"pcursor":@"",@"os":@"android",@"client_key":@"6bcc9821",@"network":@"WIFI",@"sig":@"3e20376180f73316e0e6e4acdfd6c85d"};
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
+
+ //通过post请求数据
     [manager POST:Kuaishou_URL parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"%@",operation.responseString);
+        NSLog(@"%@",operation.responseData);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
          NSLog(@"%@",error.description);
     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)uploadMoreImage:(id)sender {
+    UIImage *image = [UIImage imageNamed:@"0.png"];
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.5);
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *parameters = @{@"username": @"zhangcheng0111",@"password":@"123456"};
+    [manager POST:@"http://10.0.8.8/sns/my/upload_photo.php" parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        [formData appendPartWithFormData:imageData name:@"attach"];
+    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Success: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    
 }
+
+#define kMP4URL @"http://ws.a.yximgs.com/upic/2015/05/11/21/BMjAxNTA1MTEyMTA1MDlfNjk2MDMzM18yMjE3NjA0NjZfMV8z.mp4"
+- (IBAction)downLoadMP4File:(id)sender {
+    
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:kMP4URL]];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    
+    //设置存储路径
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *path = [[paths objectAtIndex:0] stringByAppendingPathComponent:kMP4URL.lastPathComponent];
+    operation.outputStream = [NSOutputStream outputStreamToFileAtPath:path append:NO];
+    
+    //设置下载进度检测
+    [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+        NSLog(@"bytesRead: %u, totalBytesRead: %lld, totalBytesExpectedToRead: %lld", bytesRead, totalBytesRead, totalBytesExpectedToRead);
+    }];
+    
+    //检测是否下载成功
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Successfully downloaded file to %@", path);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    
+    [operation start];
+    
+}
+
 
 @end
